2c15081ae7fcd39aa5844003c86e6ce9f57d2ad6
victor.santos <victor.santos@gmail.com>
2024-01-30 13:44:25 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+
+import { ref, onMounted } from "vue";
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import Calendar from 'primevue/calendar';
+
+
+const eventos = [new Date(2024, 0, 10), new Date(2024, 0, 15)];
+
+onMounted(() => {
+
+  const modal = document.querySelector('#modal') as HTMLElement;
+  const tds = document.querySelectorAll('td');
+
+  tds.forEach(td => {
+    const span = td.querySelector('span');
+    span!.addEventListener('mouseenter', (event) => {
+      const { clientX, clientY } = event;
+
+      modal!.style.left = `${clientX}px`;
+      modal!.style.top = `${clientY}px`;
+
+      modal.classList.add('show');
+    });
+    span!.setAttribute('style', 'cursor: pointer;');
+
+
+    span!.addEventListener('mouseleave', (event) => {
+      modal.classList.remove('show');
+    });
+  })
+
+
+})
+
+</script>
+
+<template>
+  <div>
+    <Calendar inline selectionMode="multiple" :modelValue="eventos" id="calendario"
+      @date-select="console.log('clicou')" />
+  </div>
+
+  <div id="modal" class="hidden">
+    <h2>Título do Modal</h2>
+    <p>Conteúdo do modal...</p>
+    <button id="fechar-modal">Fechar</button>
+  </div>
+</template>
+
+<style>
+td:hover {
+  color: tomato;
+}
+
+
+#modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s linear 0.2s, opacity 0.2s linear;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 20px;
+}
+
+#modal.show {
+  visibility: visible;
+  opacity: 1;
+  transition: visibility 0s linear 0.2s, opacity 0.2s linear;
+
+
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import PrimeVue from "primevue/config";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.mount("#app");

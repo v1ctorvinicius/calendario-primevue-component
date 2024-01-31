926d5f936bc4568a043c1a3683b7adf535f985f4
victor.santos <victor.santos@gmail.com>
2024-01-31 12:04:03 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,95 @@
 <script setup lang="ts">
 
-import { ref, onMounted } from "vue";
-import 'primevue/resources/themes/lara-light-blue/theme.css'
+import { ref, onMounted, onUpdated, onBeforeUpdate, watch } from "vue";
+import 'primevue/resources/themes/md-dark-deeppurple/theme.css'
 import Calendar from 'primevue/calendar';
 
 
 const eventos = [new Date(2024, 0, 10), new Date(2024, 0, 15)];
 const dias = eventos.map(data => data.getDate());
-console.log(dias);
+const meses = eventos.map(data => data.getMonth());
 
+let _tds;
 
-let _calendario;
+const teste = ref(0);
 
+const showModal = ref(false)
 
-onMounted(() => {
-
-
-  _calendario = document.querySelector('#calendario') as HTMLElement;
-
-  const modal = document.querySelector('#modal') as HTMLElement;
-  const tds = document.querySelectorAll('td');
-
-  tds.forEach(td => {
-    const span = td.querySelector('span');
-
-    span!.addEventListener('mouseenter', (event) => {
-
-      const { clientX, clientY } = event;
-
-      modal!.style.left = `${clientX}px`;
-      modal!.style.top = `${clientY}px`;
-
-      modal.classList.add('show');
-    });
-    span!.setAttribute('style', 'cursor: pointer;');
-
-
-    span!.addEventListener('mouseleave', (event) => {
-      modal.classList.remove('show');
-    });
-  })
-
-
+const position = ref({
+  left: 0,
+  top: 0
 })
 
+onBeforeUpdate(() => {
+  console.log("Antes de atualizar");
+})
+
+onMounted(() => {
+  // setup();
+})
+
+// function setup() {
+//   const modal = document.querySelector('#modal') as HTMLElement;
+//   _tds = document.querySelectorAll('td');
+
+//   _tds.forEach(td => {
+//     const span = td.querySelector('span');
+
+//     span!.addEventListener('mouseenter', (event) => {
+//       const targetElement = event.target! as HTMLElement;
+//       console.log("Elemento: ", targetElement.innerHTML);
+
+//       if (dias.includes(Number.parseInt(targetElement.innerHTML))) {
+//         console.log("É AQUI");
+
+//         const { clientX, clientY } = event;
+
+//         modal!.style.left = `${clientX}px`;
+//         modal!.style.top = `${clientY}px`;
+
+//         modal.classList.add('show');
+//       }
+//     });
+
+//     span!.addEventListener('mouseleave', (event) => {
+//       modal.classList.remove('show');
+//     });
+
+//     span!.setAttribute('style', 'cursor: pointer;');
+//   })
+
+
+
+
+
+function execute(valor: number, event: MouseEvent) {
+  if (dias.includes(valor)) {
+    position.value.left = event.clientX;
+    position.value.top = event.clientY;
+    showModal.value = true
+  }
+}
 </script>
 
 <template>
   <div>
     <Calendar id="calendario" locale="pt" dateFormat="dd/mm/yyyy" inline selectionMode="multiple" :modelValue="eventos"
-      @date-select="console.log($event)" />
+      @date-select="console.log($event)" :pt="{}" @month-change="() => { teste++; console.log(teste) }">
+
+      <template #date="slotProps">
+
+        <div @mouseover="($event) => execute(slotProps.date.day, $event)" @mouseleave="showModal = false"
+          style=" width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
+          {{ slotProps.date.day }}
+        </div>
+
+      </template>
+
+    </Calendar>
+
   </div>
 
-  <div id="modal" class="hidden">
+  <div id="modal" :class="{ show: showModal }" :style="{ left: position.left + 'px', top: position.top + 'px' }">
     <h2>Título do Modal</h2>
     <p>Conteúdo do modal...</p>
     <button id="fechar-modal">Fechar</button>
